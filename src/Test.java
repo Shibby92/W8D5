3f4191d9d192a1b7edf1a24cb6d88e144aed0799
Shibby92 <haris_000@hotmail.com>
2015-01-09 13:58:54 +0100

    W8D5

---
+++ src/Test.java
@@ -0,0 +1,31 @@
+/**
+ * Testiranje liked liste i array liste
+ * 
+ * @author harisarifovic
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		LinkedListIn test = new LinkedListIn();
+		test.add(2);
+		test.add(4);
+		test.add(6);
+		test.add(99,1);
+		test.add(100,4);
+		System.out.println(test.toString());
+		test.remove(2);
+		System.out.println(test.toString());
+		ArrayListInt test2 = new ArrayListInt();
+		test2.add(1);
+		test2.add(3);
+		test2.add(5);
+		test2.add(99, 1);
+		test2.add(100, 2);
+		test2.add(500,5);
+		System.out.println(test2.toString());
+		test2.remove(2);
+		System.out.println(test2.toString());
+	}
+
+}
